f297e409afa9765a1072cf1aea4ae6f9e6f7c3a2
NGUITCHOU <nguitchoumurielle@gmail.com>
2025-03-20 10:21:42 -0700

    add updated files to math_setup

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Home = () => {
-  return (
-        <div>
-            <h1>Welcome to the home page</h1>
+const Home = () => (
+        <div className='home'>
+            <h1>Welcome to our page!</h1>
+            <h2>Unlock the World of Mathematics</h2>
+            <p>Mathematics is more than just numbers and equation; its a
+                universal language that empowers us to understand and
+                interact with the world around us. Whether you are a student,
+                a teacher, or just someone who wants to learn more about the
+                fascinating world of mathematics, we have the resources you
+                need to explore the subject.
+            </p>
         </div>
-  );
-};
+);
 
 export default Home;
--- src/components/quotes.jsx
+++ src/components/quotes.jsx
@@ -4,49 +4,50 @@ const Quote = () => {
   const [quotes, setQuotes] = useState([]);
   const [error, setError] = useState(null);
 
-  const fetchQuotes = async() =>{
-      try {
-        const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
-          method: 'GET',
-          headers: {
-            'X-Api-Key': 'VBYixFI8vUY8+Gu8yob7iw==wyzA3zkD7bM06NC2', 
-            'Content-Type': 'application/json',
-          },
-        });
+  const fetchQuotes = async () => {
+    try {
+      const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
+        method: 'GET',
+        headers: {
+          'X-Api-Key': 'hwO894pl2wS4hx7F2jZjVQ==wmZG2EEAD5bzteTg',
+          'Content-Type': 'application/json',
+        },
+      });
 
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-        setQuotes(data);
-      } catch (err) {
-        console.error(err);
-        setError(err.message);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
+
+      const data = await response.json();
+      setQuotes(data);
+    } catch (err) {
+      error(err);
+      setError(err.message);
+    }
+  };
+  useEffect(() => {
+    const quotid = setInterval(() => {
+      fetchQuotes();
+    },
+    10000);
+    return () => {
+      clearInterval(quotid);
     };
-useEffect(()=>{
-    const quotid = setInterval(() =>{
-    fetchQuotes();
-},
- 20000);
-return()=>{
-    clearInterval(quotid);
-};
-}, []);
+  }, []);
   return (
         <div>
         <h1>Welcome to Quote page</h1>
-        <h1>Quotes</h1>
         {error && <p style={{ color: 'red' }}>Error: {error}</p>}
         <ul>
             {quotes.map((quote, index) => (
                 <li key={index}>
-                    <strong>{quote.quote}</strong> - {quote.author}
+                <h4> {quote.author}</h4>
+                    <h5>{quote.quote}</h5>
                 </li>
             ))}
         </ul>
     </div>
   );
 };
+
 export default Quote;
